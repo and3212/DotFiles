9d26aea00ef227046356422646a5161886cb03a4
Liam Lawrence <and3212@gmail.com>
2017-09-24 20:59:06 -0400

    Updated file to have a choice whether or not you use the clobber feature

--- bin/getWebsite.sh
+++ bin/getWebsite.sh
@@ -6,4 +6,13 @@
 echo "Enter the name of the website you would like to download"
 read webName
 
-wget --recursive --no-clobber --page-requisites --convert-links --restrict-file-names=windows --no-clobber $webName
+read -sp "Do you want to overwrite previously downloaded files from this website? [y/N]" -n 1 -r
+REPLY=${REPLY,,}
+
+if [ "$REPLY" != "y" ]; then
+    wget --recursive --page-requisites --convert-links --restrict-file-names=windows --no-clobber $webName
+else
+    wget --recursive --page-requisites --convert-links --restrict-file-names=windows $webName
+fi
+
+exit
