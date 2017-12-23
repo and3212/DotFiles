088f280d763088e684146099ef77cff12a87de7d
Liam Lawrence <and3212@gmail.com>
2017-12-23 15:28:11 -0500

    Update pdfScrapper.sh

--- bin/pdfScrapper.sh
+++ bin/pdfScrapper.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
 # Author: Liam Lawrence
 # Date: December 23, 2017
-# Rips out information from PDFs to make pinouts easier for PCB design
+# Creates a .CSV file from an Atmel pinout PDF
 
 FPAGE=$1
 LPAGE=$2
 FILEPATH=$3
 NAME=$(basename $FILEPATH)
 NAME=${NAME%.*}
+CONFIGFILE=$4
 
 # Gets the file data and stores it in pdfName.txt
 pdftotext -f $FPAGE -l $LPAGE -layout -nopgbrk -eol "unix" $FILEPATH $NAME.txt
 
-# Removes the last word
-WORD=$(cat "$NAME.txt" |  
-    sed -e 's/  */ /g' |
-    sed -e 's# O PIO, I, PU, ST##' | 
-    sed -e 's# I PIO, I, PU, ST##' | 
-    sed -e 's# PIO, I, PU, ST##')
+# Removes extra spaces and applies the config file
+WORD=$(cat "$NAME.txt" | sed -e 's/  */ /g')
+while read line; do
+    WORD=$(echo "$WORD" | sed -e "$line")
+done < <(cat "$CONFIGFILE")
+
 
 # Replaces the last word with the I/O data
 WORD=$(echo "$WORD" | sed 's/.*/& x/' | awk '{$NF=$4}1' | cut -d ' ' -f4 --complement)
 
+
 # Deletes the old text file and creates a .CSV
 rm $NAME.txt
-touch $NAME.csv
+> $NAME.csv
+
 
 # Places / between the description words
-echo "$WORD" | while read line ; do
+# and writes out to a .CSV file
+echo "$WORD" | while read line; do
+
     # Finds the number of words in each line - 2
     COUNTER=$(echo "$line" | wc -w)
     let COUNTER=COUNTER-2
@@ -39,4 +44,5 @@ echo "$WORD" | while read line ; do
 
     # Adds it to the file
     echo ${line// /,} >> $NAME.csv
+
 done
